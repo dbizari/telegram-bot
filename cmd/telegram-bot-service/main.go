19fef6041be9df9517b8a7f1f512a77b86e5360a
Daniel Bizari <bizdanie@amazon.com>
2022-10-29 00:20:26 -0300

    add telegram bot lib dependency

--- cmd/telegram-bot-service/main.go
+++ cmd/telegram-bot-service/main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
 	"os"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("it works!"))
-	})
+	//mux := http.NewServeMux()
+	//mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	//	writer.Write([]byte("it works!"))
+	//})
+	//
+	//port := os.Getenv("PORT")
+	//fmt.Println("listening on port " + port)
+	//
+	//http.ListenAndServe(":"+port, mux)
 
-	port := os.Getenv("PORT")
-	fmt.Println("listening on port " + port)
+	telegramToken := os.Getenv("TELEGRAM_TOKEN")
+	bot, err := tgbotapi.NewBotAPI(telegramToken)
+	if err != nil {
+		log.Panic(err)
+		return
+	}
 
-	http.ListenAndServe(":"+port, mux)
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
